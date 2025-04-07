d24123da2e83f6589df4cd826669120c9d30af1f
JosueRafP <josue.petri.senac@gmail.com>
2025-04-07 16:38:03 -0300

    Movimentação do jogador sendo feita

---
+++ PI - 2025/Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMoviment : MonoBehaviour
+{
+    [SerializeField] float horizontal, vertical, speed;
+    Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
+}
