d61756ec4c7a7ae8cee761fd045ad9d61e2cf54a
JosueRFP <josue.petri.senac@gmail.com>
2025-04-14 16:30:14 -0300

    Puzzle dos botões feitos
    Troquei o puzzle de caixas para um de botão (mais facil do que pensei)

---
+++ PI - 2025/Assets/Scripts/ButtonPuzzle.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+[RequireComponent (typeof(BoxCollider2D))]
+public class ButtonPuzzle : MonoBehaviour
+{
+    [SerializeField] static string correctOrder = "3312";
+    [SerializeField] static string playerOrder = "";
+
+    [SerializeField] static int digitsTotal = 0;
+    [SerializeField] GameObject gate;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(playerOrder);
+
+        if (digitsTotal == 4)
+        {
+            if (playerOrder == correctOrder)
+            {
+                print("Correto!");
+                Instantiate(gate);
+
+            }
+            else
+            {
+                playerOrder = "";
+                digitsTotal = 0;
+                print("Incorreto");
+                
+
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        playerOrder += gameObject.name;
+        digitsTotal += 1;
+    }
+
+}
+// video referencia https://www.youtube.com/watch?v=o7aC62Nsv-M
