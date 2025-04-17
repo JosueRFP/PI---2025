c343268c20142a2c4d30ae3760539d19531ccb98
Arthur Bonelli <artbonelli@gmail.com>
2025-04-17 15:12:21 -0300

    sdaassaassa

---
+++ PI - 2025/Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject gate;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gate.SetActive(false);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gate.SetActive(true);
+        }
+    }
+}
