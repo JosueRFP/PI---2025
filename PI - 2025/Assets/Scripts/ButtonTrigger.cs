b844014f65e35d027f7429e108df8550606f267f
Arthur Bonelli <artbonelli@gmail.com>
2025-05-08 10:44:07 -0300

    dasas

--- PI - 2025/Assets/Scripts/ButtonTrigger.cs
+++ PI - 2025/Assets/Scripts/ButtonTrigger.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ButtonTrigger : MonoBehaviour
 {
+    [SerializeField] string tagg;
     [SerializeField] GameObject gate;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,14 +17,14 @@ public class ButtonTrigger : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag(tagg))
         {
             gate.SetActive(false);
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag(tagg))
         {
             gate.SetActive(true);
         }
